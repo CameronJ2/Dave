5b9fea113edb02c422b7af18b148b124c56a4ba3
CameronJ2 <jonescameron980p@gmail.com>
2024-07-18 23:34:45 -0500

    git add .

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 // import express from 'express'
 // const {Client, IntentsBitField} = require('discord.js')
-import {Client, IntentsBitField} from 'discord.js'
-import {DisTube} from 'distube'
-import {joinVoiceChannel} from 'discord.js/src/client/voice'
-import {exec} from 'child_process'
-import dotenv from 'dotenv'
-import { join } from 'path'
+import { Client, IntentsBitField } from "discord.js"
+import { DisTube } from "distube"
+import { joinVoiceChannel } from "discord.js/src/client/voice"
+import { exec } from "child_process"
+import dotenv from "dotenv"
+import { join } from "path"
 dotenv.config()
 
-const token = process.env.DISCORD_TOKEN;
+const token = process.env.DISCORD_TOKEN
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds , 
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.GuildVoiceStates,
-        IntentsBitField.Flags.MessageContent
-    ]
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildVoiceStates,
+    IntentsBitField.Flags.MessageContent,
+  ],
 })
 
 client.DisTube = new DisTube(client, {
   leaveOnStop: true,
   emitNewSongOnly: true,
   emitAddSongWhenCreatingQueue: false,
-  emitAddListWhenCreatingQueue: false
+  emitAddListWhenCreatingQueue: false,
 })
 
-client.on('ready', function(client) {
-    console.log(`${client.user.tag} is ready`)
+client.on("ready", function (client) {
+  console.log(`${client.user.tag} is ready`)
 })
 
-client.login(token);
+client.on("messageCreate", (msg) => {
+  if (msg.author.bot) return
+  const prefix = "?"
+
+  if (!msg.content.startsWith("?")) return
+})
+
+client.login(token)
