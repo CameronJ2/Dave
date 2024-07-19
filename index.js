af560b309dd3788f2cf0c2f088c5a5848660e48f
Chris Amori <chris.amori93@gmail.com>
2024-07-18 23:24:41 -0700

    Testing different discord-player options

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // import express from 'express'
 // const {Client, IntentsBitField} = require('discord.js')
 import { Client, IntentsBitField } from "discord.js"
-import { DisTube } from "distube"
-import { joinVoiceChannel } from "discord.js/src/client/voice"
+import { joinVoiceChannel } from "@discordjs/voice"
+import { getVoiceConnection } from "@discordjs/voice"
 import { exec } from "child_process"
 import dotenv from "dotenv"
 import { join } from "path"
+import { Player, QueryType } from "discord-player"
+import { YouTubeExtractor } from "@discord-player/extractor"
 dotenv.config()
 
 const token = process.env.DISCORD_TOKEN
@@ -19,22 +21,50 @@ const client = new Client({
   ],
 })
 
-client.DisTube = new DisTube(client, {
-  leaveOnStop: true,
-  emitNewSongOnly: true,
-  emitAddSongWhenCreatingQueue: false,
-  emitAddListWhenCreatingQueue: false,
-})
+const player = new Player(client)
 
 client.on("ready", function (client) {
   console.log(`${client.user.tag} is ready`)
 })
 
-client.on("messageCreate", (msg) => {
+client.on("messageCreate", async (msg) => {
   if (msg.author.bot) return
   const prefix = "?"
+  const channel = msg.member.voice.channel
+  const query = msg.content.slice(prefix.length).trim()
 
   if (!msg.content.startsWith("?")) return
+
+  await player.extractors.loadDefault()
+
+  try {
+    const { track } = await player.play(channel, query, {
+      nodeOptions: {
+        // nodeOptions are the options for guild node (aka your queue in simple word)
+        // metadata: interaction, // we can access this metadata object using queue.metadata later on
+      },
+    })
+
+    console.log(`**${track.title}** enqueued!`)
+  } catch (e) {
+    // let's return error if something failed
+    console.log(`Something went wrong: ${e}`)
+  }
+
+  //   if (msg.content.startsWith("?join")) {
+  //     const connection = joinVoiceChannel({
+  //       channelId: msg.member.voice.channel.id,
+  //       guildId: msg.member.voice.channel.guildId,
+  //       adapterCreator: msg.member.voice.channel.guild.voiceAdapterCreator,
+  //       selfDeaf: false,
+  //     })
+  //   }
+  //   if (msg.content.startsWith("?leave")) {
+  //     const connection = getVoiceConnection(msg.member.voice.channel.guildId)
+  //     connection.destroy()
+  //   }
+  //   if (msg.content.startsWith("?play")) {
+  //   }
 })
 
 client.login(token)
