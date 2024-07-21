ef4a5a6369e0a9600113b5605958e5b4615db229
Chris Amori <chris.amori93@gmail.com>
2024-07-20 22:20:36 -0700

    Command Library (#1)
    * command library
    
    * Fix command
    
    * Break logic into 2 lines
    
    * Rename var
    
    * Error handling

---
+++ commands/play.js
@@ -0,0 +1,21 @@
+import { distubeInstance } from "../index.js"
+
+export default async (args, msg) => {
+  const query = args.join(" ")
+  const voiceChannel = msg.member.voice.channel
+
+  if (!voiceChannel) {
+    return msg.channel.send("You need to be in a voice channel to play music!")
+  }
+
+  try {
+    await distubeInstance.play(voiceChannel, query, {
+      member: msg.member,
+      textChannel: msg.channel,
+      message: msg,
+    })
+  } catch (error) {
+    console.error(error)
+    msg.channel.send("An error occurred while trying to run distube.play()")
+  }
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import { DisTube } from "distube"
 import { YtDlpPlugin } from "@distube/yt-dlp"
 import { YouTubePlugin } from "@distube/youtube"
+import { runCommand } from "./commands/index.js"
 dotenv.config()
 
 const token = process.env.DISCORD_TOKEN
@@ -16,11 +17,7 @@ const client = new Client({
   ],
 })
 
-const distubeOptions = {
-  plugins: [new YouTubePlugin(), new YtDlpPlugin()],
-}
-
-const distube = new DisTube(client, {
+export const distubeInstance = new DisTube(client, {
   plugins: [new YouTubePlugin(), new YtDlpPlugin()],
 })
 
@@ -28,33 +25,11 @@ client.on("ready", function (client) {
   console.log(`${client.user.tag} is ready`)
 })
 
-client.on("messageCreate", async (msg) => {
-  if (msg.author.bot) return
-  if (!msg.content.startsWith("?")) return
-
-  const prefix = "?"
-  const args = msg.content.slice(prefix.length).trim().split(/ +/g)
-
-  const query = args.join(" ")
-  const voiceChannel = msg.member.voice.channel
-
-  if (!voiceChannel) {
-    return msg.channel.send("You need to be in a voice channel to play music!")
-  }
-
-  try {
-    await distube.play(voiceChannel, query, {
-      member: msg.member,
-      textChannel: msg.channel,
-      message: msg,
-    })
-  } catch (error) {
-    console.error(error)
-    msg.channel.send("An error occurred while trying to play the song.")
-  }
+client.on("messageCreate", function (msg) {
+  runCommand(msg)
 })
 
-distube.on("playSong", (queue, song) => {
+distubeInstance.on("playSong", function (queue, song) {
   queue.textChannel.send(`Now playing: ${song.name} - ${song.url}`)
 })
 
