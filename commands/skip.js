288945bacae3aa637388cb152afa385c4c602860
Chris Amori <chris.amori93@gmail.com>
2024-07-22 23:28:02 -0700

    queue lie

--- commands/skip.js
+++ commands/skip.js
@@ -1,7 +1,8 @@
 import { distubeInstance } from "../index.js"
 
 export default async (args, msg) => {
-  const songName = distubeInstance.getQueue(msg.guild).songs[0].name
+  const queue = distubeInstance.getQueue(msg.guild)
+  const songName = queue.songs[0].name
   await distubeInstance.skip(msg.guildId)
   await msg.channel.send(`Skipped ${songName}`)
 }
