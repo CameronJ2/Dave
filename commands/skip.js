6ae07ccbae2025aaa83bab499214f72f1259c36c
Chris Amori <chris.amori93@gmail.com>
2024-07-22 23:08:20 -0700

    Feedback on skip

--- commands/skip.js
+++ commands/skip.js
@@ -1,5 +1,12 @@
 import { distubeInstance } from "../index.js"
 
 export default async (args, msg) => {
-  await distubeInstance.skip(msg.guildId)
+  try {
+    const songName = distubeInstance.getQueue(msg.guild)?.songs?.[0]?.name
+    await distubeInstance.skip(msg.guildId)
+    await msg.channel.send(`Skipped ${songName}`)
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
 }
