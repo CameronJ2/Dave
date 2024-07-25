c8089ac5e754951a0c2b0cde553e6e54dcc97c03
CameronJ2 <jonescameron980p@gmail.com>
2024-07-24 21:07:47 -0500

    Deleted old JS files

--- commands/skip.js
+++
@@ -1,8 +0,0 @@
-import { distubeInstance } from "../index.js"
-
-export default async (args, msg) => {
-  const queue = distubeInstance.getQueue(msg.guild)
-  const songName = queue.songs[0].name
-  await distubeInstance.skip(msg.guildId)
-  await msg.channel.send(`Skipped ${songName}`)
-}
