159920802a49b9d147f4f926c21bfeb1ed0c207f
CameronJ2 <jonescameron980p@gmail.com>
2024-07-31 15:01:37 -0500

    Functional, but doesn't play the song again if it ffmpeg errors.

--- commands/play.ts
+++ commands/play.ts
@@ -1,34 +1,10 @@
 import type { GuildTextBasedChannel, Message } from "discord.js"
-import { DisTubeEvents, Events } from "distube"
-import { distubeInstance, client } from "../index.js"
 import { ActivityType } from "discord.js"
+import { DisTubeEvents, Events } from "distube"
+import { client, distubeInstance } from "../index.js"
 
-// const errorListenerFactory = (args: string[], msg: Message, retries = 0) => {
-//   return async (error: Error) => {
-//     if (retries >= 3) {
-//       console.error("Too many retries!")
-//       return
-//     }
-
-//     console.log("In error event listener:\n")
-//     console.error({ name: error.name, message: error.message })
-
-//     if (error.name !== "DisTubeError [FFMPEG_EXITED]") {
-//       return
-//     }
-
-//     console.error("FFMPEG error caught")
-//     await play(args, msg, retries + 1)
-//   }
-// }
-
-let LASTMESSAGE: Message<boolean>
-let LASTQUERY: string
-
-export const play = async (args: string[], msg: Message) => {
-  LASTMESSAGE = msg
+export const play = async (args: string[], msg: Message): Promise<unknown> => {
   const query = args.join(" ")
-  LASTQUERY = query
   const voiceChannel = msg.member?.voice.channel
 
   if (!voiceChannel) {
@@ -62,25 +38,9 @@ export const play = async (args: string[], msg: Message) => {
       `Added song to queue: ${lastSongInQueue.name} - ${lastSongInQueue.url}`
     )
   } catch (error) {
-    console.error(error)
+    console.error({ error, message: error.message })
     msg.channel.send("An error occurred while trying to run distube.play()")
   }
 
   return msg
 }
-
-export const replay = async () => {
-  const msg = LASTMESSAGE
-  const query = LASTQUERY
-  const voiceChannel = msg.member?.voice.channel
-  if (!voiceChannel) {
-    return msg.channel.send("You need to be in a voice channel to play music!")
-  }
-  await distubeInstance.play(voiceChannel, query, {
-    member: msg.member,
-    textChannel: msg.channel as GuildTextBasedChannel,
-    message: msg,
-  })
-}
-
-// export default play
